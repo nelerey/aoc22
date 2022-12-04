5c25e87736dd30000c751b70525d0b6699bce4e0
Nele Reyniers <nele.reyniers@live.be>
2022-12-04 09:19:22 +0000

    IK STA VOOR OP SONCK\!\!\!

--- code/day4.f90
+++ code/day4.f90
@@ -1,16 +1,12 @@
-! let the record state that I keep using the .f90 extension but I have no idea which fortran version I'm actually working with
-! alsook, logical to integer conversion bestaat niet en/of is niet altijd betrouwbaar ofzo?!
-! tijd om eens te leren hoe functies en subroutines werken eh nele vinde ook nie da had gisteren eigenlijk wel nuttig geweest eh nele
-
 program cleaningAssignments
     character(20)   :: filepath="../data/input_d4.txt"
     character(20)    :: line
-    integer         :: elf1(1000, 2), elf2(1000, 2), contained(1000) !, !elf1in2(1000), elf2in1(1000)
-    logical, dimension(1000) :: elf1in2_1, elf1in2_2, elf2in1_1, elf2in1_2, elf1in2, elf2in1
+    integer         :: elf1(1000, 2), elf2(1000, 2), contained(1000), overlapping(1000)
+    logical, dimension(1000) :: elf1in2, elf2in1
+    logical, dimension(1000) :: elf1lap2,  elf2lap1
 
-
+    ! read data
     open(1, file=filepath, status='old', action="read")
-    ! store all pair starts and pair ends in vectors
     do i=1,1000
         read(1,'(A)', end=1) line ! elf1(i,:), elf2(i,:)
         ! making integers
@@ -21,25 +17,27 @@ program cleaningAssignments
 
     end do
 1   close(1)
-
-    ! print *, line( : index(line, "-")-1)
-    ! print *, line(index(line, "-")+1 : index(line, ",")-1)
-    ! print *, line(index(line, ",")+1 : index(line, "-", back=.true.)-1)
-    ! print *, line(index(line, "-", back=.true.)+1 : )
-
-    ! print *, elf1
-    ! print *, elf2
     
+    ! part 1
     elf1in2 = (elf1(:,1) >= elf2(:,1)) .and. (elf1(:,2) <= elf2(:,2))
     elf2in1 = (elf2(:,1) >= elf1(:,1)) .and. (elf2(:,2) <= elf1(:,2))
-    
     where (elf1in2 .or. elf2in1)
         contained = 1
     elsewhere
         contained = 0
     end where
+    print *, "n contained: ", sum(contained)
 
-    print *, sum(contained)
+    ! part 2
+    elf1lap2 = ((elf1(:,1) >= elf2(:,1)) .and. (elf1(:,1) <= elf2(:,2)))   ! start of elf 1 in the range of elf 2
+    elf2lap1 = ((elf2(:,1) >= elf1(:,1)) .and. (elf2(:,1) <= elf1(:,2)))   ! start of elf 2 in the range of elf 1
+
+    where (elf1lap2 .or. elf2lap1)
+        overlapping = 1
+    elsewhere
+        overlapping = 0
+    end where
+    print *, "n overlapping: ", sum(overlapping)
 
 end program cleaningAssignments
-
+! deze cleaning assignments hadden properder gekund.
